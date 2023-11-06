a76076ef87f15b7b0524d9c107dc5a2c732df4a8
Guillaume De Martino <guillaumedemartino@gmail.com>
2023-11-06 18:17:06 +0100

    Lock mutation

--- src/graphql/Lock.ts
+++ src/graphql/Lock.ts
@@ -1,4 +1,4 @@
-import { objectType } from "nexus";
+import { extendType, intArg, nonNull, objectType, stringArg } from "nexus";
 
 export const Lock = objectType({
   name: "Lock",
@@ -23,5 +23,82 @@ export const Lock = objectType({
         return context.prisma.lock.findFirst({ where: { lockTargetByTxid: parent.lockTargetByTxid}}).lockTarget()
       }
     }) 
+  },
+})
+
+// TODO Ranking Query
+
+interface NewLockProps {
+  txid: string;
+  createdAt: string;
+  satoshis: number;
+  blockHeight: number;
+  lockTargetByTxid: string;
+  lockerByUserAddress: string;
+  lockerByUserPaymail: string;
+}
+
+export const LockMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("lock", {
+      type: "Lock",
+      args: {
+        txid: nonNull(stringArg()),
+        createdAt: stringArg(),
+        satoshis: nonNull(intArg()),
+        blockHeight: nonNull(intArg()),
+        lockTargetByTxid: nonNull(stringArg()),
+        lockerByUserAddress: nonNull(stringArg())
+      },
+      async resolve(parent, args: NewLockProps, context) {
+        const { txid, createdAt, satoshis, blockHeight, lockTargetByTxid, lockerByUserAddress, lockerByUserPaymail } = args
+
+        /* const { userId } = context;
+        
+        if (!userId) {
+            throw new Error("Cannot post without logging in.");
+        } */
+
+        const newLock = context.prisma.lock.create({
+          data: {
+            transaction: {
+              connectOrCreate: {
+                where: {
+                  hash: txid
+                },
+                create: {
+                  hash: txid
+                }
+              },
+            },
+            createdAt,
+            satoshis,
+            blockHeight,
+            lockTarget: {
+              connectOrCreate: {
+                where: {
+                  hash: lockTargetByTxid
+                },
+                create: {
+                  hash: lockTargetByTxid
+                }
+              }
+            },
+            locker: {
+              connectOrCreate: {
+                where: {
+                  address: lockerByUserAddress
+                },
+                create: {
+                  address: lockerByUserAddress,
+                  paymail: lockerByUserPaymail
+                }
+              }
+            }
+          }
+        })
+      }
+    })
   },
 })
