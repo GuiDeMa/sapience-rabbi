161e4a328e272ab7f59e84ba39a74ef45e975218
Guillaume De Martino <guillaumedemartino@gmail.com>
2023-11-20 13:56:51 +0000

    add debugging

--- src/actors/ingest_lockup_transactions/actor.ts
+++ src/actors/ingest_lockup_transactions/actor.ts
@@ -140,7 +140,7 @@ export async function start(){
             }
         }
 
-        
+        console.log("lock target", targetBmapTx)
         const response = await prisma.lock.upsert({
             where: { txid },
             create: {
